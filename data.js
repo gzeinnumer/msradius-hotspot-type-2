6966fd56528a17c7569d1df07f56d65fee590212
chairul <rahman9868@gmail.com>
2025-04-29 19:06:28 +0700

    data

--- data.js
+++ data.js
@@ -1,7 +1,25 @@
-data = '[{"nama" : "MSRadius Hotspot"}, {"alamat" : "Jl. Tanah Merah IV Gang Kemangi No. 41"}, {"phone" : "082212345678"}, {"keterangan" : "Voucher Kemangi 41 bisa di beli di gang Kemangi No.41 atau gang Kluweh No.19 (Rosi)"}, {"copyright" : "2025 by Chairul Rahman"}, {"masaaktif" : ""}, {"trial" : "Coba Gratis"}, {"notifikasi" : "Internet dalam perbaikan."}]';
+base_url = "https://demo.msradius.com";
 
-datatitle = '[{"title1" : "Paket Wifi"}, {"title2" : "Voucher"},{"title3" : "Alamat"}, {"title4" : "Kontak"}, {"title5" : "Peringatan"}, {"title6" : "Expired"}]';
+nama = "MSRadius Hotspot";
+alamat = "Jl. Tanah Merah IV Gang Kemangi No. 41";
+phone = "082212345678";
+keterangan = "Voucher Kemangi 41 bisa di beli di gang Kemangi No.41 atau gang Kluweh No.19 (Rosi)";
+copyright = "2025 by Chairul Rahman";
+masaaktif = "";
+trial = "Coba Gratis";
+notifikasi = "Internet dalam perbaikan.";
 
-dataerr = '[{"expired" : "Kode Voucher atau User yang Anda masukkan sudah expired. Silahkan gunakan Kode Voucher atau User yang masih berlaku."}, {"invaliduser" : "Kode Voucher atau User yang Anda masukkan tidak sesuai. Silahkan masukkan kembali dengan benar."}, {"sudahlogin" : "User yang Anda masukkan sudah login, Anda tidak dapat login untuk yang kedua kalinya. Silahkan tunggu 2 menit Kode Voucher atau User Anda akan logout secara otomatis. Jika tidak, maka kemungkinan besar Kode Voucher atau User Anda digunakan oleh orang lain. Jagalah kerahasiaan Kode Voucher atau User Anda!"}, {"ilegal" : "Anda tidak bisa login menggunakan device ini. Silahkan gunakan device yang terdaftar."}, {"radius" : "RADIUS server tidak merespon."}]';
+title1 = "Paket Wifi";
+title2 = "Voucher";
+title3 = "Alamat";
+title4 = "Kontak";
+title5 = "Peringatan";
+title6 = "Expired";
+
+expired = "Kode Voucher atau User yang Anda masukkan sudah expired. Silahkan gunakan Kode Voucher atau User yang masih berlaku.";
+invaliduser = "Kode Voucher atau User yang Anda masukkan tidak sesuai. Silahkan masukkan kembali dengan benar.";
+sudahlogin = "User yang Anda masukkan sudah login, Anda tidak dapat login untuk yang kedua kalinya. Silahkan tunggu 2 menit Kode Voucher atau User Anda akan logout secara otomatis. Jika tidak, maka kemungkinan besar Kode Voucher atau User Anda digunakan oleh orang lain. Jagalah kerahasiaan Kode Voucher atau User Anda!";
+ilegal = "Anda tidak bisa login menggunakan device ini. Silahkan gunakan device yang terdaftar.";
+radius = "RADIUS server tidak merespon.";
 
 base_url = "https://demo.msradius.com";
