0475b1d6a2d1e1a2958c7fa2dc287a906271f329
chairul <rahman9868@gmail.com>
2025-04-30 13:31:01 +0700

    dinamis view

--- data.js
+++ data.js
@@ -3,7 +3,8 @@ base_url = "https://demo.msradius.com";
 nama = "MSRadius Hotspot";
 alamat = "Jl. Tanah Merah IV Gang Kemangi No. 41";
 phone = "082212345678";
-keterangan = "Voucher Kemangi 41 bisa di beli di gang Kemangi No.41 atau gang Kluweh No.19 (Rosi)";
+keterangan =
+  "Voucher Kemangi 41 bisa di beli di gang Kemangi No.41 atau gang Kluweh No.19 (Rosi)";
 copyright = "2025 by Chairul Rahman";
 masaaktif = "";
 trial = "Coba Gratis";
@@ -16,8 +17,25 @@ title4 = "Kontak";
 title5 = "Peringatan";
 title6 = "Expired";
 
-expired = "Kode Voucher atau User yang Anda masukkan sudah expired. Silahkan gunakan Kode Voucher atau User yang masih berlaku.";
-invaliduser = "Kode Voucher atau User yang Anda masukkan tidak sesuai. Silahkan masukkan kembali dengan benar.";
-sudahlogin = "User yang Anda masukkan sudah login, Anda tidak dapat login untuk yang kedua kalinya. Silahkan tunggu 2 menit Kode Voucher atau User Anda akan logout secara otomatis. Jika tidak, maka kemungkinan besar Kode Voucher atau User Anda digunakan oleh orang lain. Jagalah kerahasiaan Kode Voucher atau User Anda!";
-ilegal = "Anda tidak bisa login menggunakan device ini. Silahkan gunakan device yang terdaftar.";
+expired =
+  "Kode Voucher atau User yang Anda masukkan sudah expired. Silahkan gunakan Kode Voucher atau User yang masih berlaku.";
+invaliduser =
+  "Kode Voucher atau User yang Anda masukkan tidak sesuai. Silahkan masukkan kembali dengan benar.";
+sudahlogin =
+  "User yang Anda masukkan sudah login, Anda tidak dapat login untuk yang kedua kalinya. Silahkan tunggu 2 menit Kode Voucher atau User Anda akan logout secara otomatis. Jika tidak, maka kemungkinan besar Kode Voucher atau User Anda digunakan oleh orang lain. Jagalah kerahasiaan Kode Voucher atau User Anda!";
+ilegal =
+  "Anda tidak bisa login menggunakan device ini. Silahkan gunakan device yang terdaftar.";
 radius = "RADIUS server tidak merespon.";
+
+logo_url = "img/logo.png";
+favicon_url = "favicon.ico";
+background_url = "img/bg.jpg";
+
+//Aktifkan Penjualan EVoucher
+aktifkan_evoucher = "Y";
+
+//Aktifkan Pendaftaran Membership
+aktifkan_daftar_membership = "Y";
+
+//Aktifkan Pendaftaran Billing
+aktifkan_daftar_billing = "Y";
